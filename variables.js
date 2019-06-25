3662309640b11ffa5499ffe05034dfb34e5a9d7b
Hisan <hisanibrahim@gmail.com>
2019-06-25 13:59:30 +0530

    variables var, let, const completed

---
+++ variables.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!")
+
+var 
+num = 5,
+name = "Hisan",
+developer = false | true,
+location = null,
+undef;
+
+console.log(name);
+
+var name = "Hisan Ibrahim"
+
+console.log(name);
+
+let x;
+x = 4;
+console.log(x);
+
+const person = {
+    name: 'Mobin'
+};
+console.log(person.name)
+
+person.name = 'Mobin Mohanan'
+
+console.log(person.name)
